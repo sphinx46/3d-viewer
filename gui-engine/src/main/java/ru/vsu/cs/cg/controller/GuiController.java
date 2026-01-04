450b92df179c36e62fd3ffeca7326c7033ea53b6
Ilya Mordvinov <morilya111@gmail.com>
2026-01-04 16:43:39 +0300

    feat: изменение светлой темы и ползунков

--- gui-engine/src/main/java/ru/vsu/cs/cg/controller/GuiController.java
+++ gui-engine/src/main/java/ru/vsu/cs/cg/controller/GuiController.java
@@ -2,26 +2,40 @@ package ru.vsu.cs.cg.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.StringConverter;
+import javafx.util.converter.NumberStringConverter;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GuiController {
-
-    @FXML
-    private AnchorPane anchorPane;
-
-    @FXML
-    private MenuItem menuThemeDark;
-
-    @FXML
-    private MenuItem menuThemeLight;
 
     private static final String DARK_THEME = "/static/css/theme-dark.css";
     private static final String LIGHT_THEME = "/static/css/theme-light.css";
 
+    @FXML private AnchorPane anchorPane;
+    @FXML private MenuItem menuThemeDark;
+    @FXML private MenuItem menuThemeLight;
+
+    @FXML private Slider brightnessSlider;
+    @FXML private TextField brightnessField;
+    @FXML private Slider reflectionSlider;
+    @FXML private TextField reflectionField;
+    @FXML private Slider focalLengthSlider;
+    @FXML private TextField focalLengthField;
+    @FXML private Slider sensitivitySlider;
+    @FXML private TextField sensitivityField;
+
+    private final Map<Slider, TextField> sliderBindings = new HashMap<>();
+
     @FXML
     private void initialize() {
         setupThemeHandlers();
+        collectSliderBindings();
+        setupAllSliderBindings();
     }
 
     private void setupThemeHandlers() {
@@ -35,5 +49,54 @@ public class GuiController {
             anchorPane.getStylesheets().clear();
             anchorPane.getStylesheets().add(themeUrl.toExternalForm());
         }
+    }
+
+    private void collectSliderBindings() {
+        sliderBindings.put(brightnessSlider, brightnessField);
+        sliderBindings.put(reflectionSlider, reflectionField);
+        sliderBindings.put(focalLengthSlider, focalLengthField);
+        sliderBindings.put(sensitivitySlider, sensitivityField);
+    }
+
+    private void setupAllSliderBindings() {
+        sliderBindings.forEach(this::bindSliderToTextField);
+    }
+
+    private void bindSliderToTextField(Slider slider, TextField textField) {
+        StringConverter<Number> converter = createConverterForSlider(slider);
+        textField.textProperty().bindBidirectional(slider.valueProperty(), converter);
+    }
+
+    private StringConverter<Number> createConverterForSlider(Slider slider) {
+        return new NumberStringConverter() {
+            @Override
+            public String toString(Number value) {
+                return formatValue(value.doubleValue(), slider);
+            }
+
+            @Override
+            public Number fromString(String string) {
+                return parseValue(string, slider);
+            }
+        };
+    }
+
+    private String formatValue(double value, Slider slider) {
+        return slider == focalLengthSlider ?
+            String.format("%.0f", value) :
+            String.format("%.2f", value);
+    }
+
+    private double parseValue(String input, Slider slider) {
+        try {
+            double value = Double.parseDouble(input.replace(',', '.'));
+            return clamp(value, slider.getMin(), slider.getMax());
+        } catch (NumberFormatException e) {
+            return slider.getValue();
+        }
+    }
+
+    private double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(value, max));
     }
 }
