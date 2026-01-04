901722fb4135bec231996394eadce8d07d950eff
Ilya Mordvinov <morilya111@gmail.com>
2026-01-04 15:44:43 +0300

    feat: реализовать базовый ui на основе fxml и css стилей

--- gui-engine/src/main/java/ru/vsu/cs/cg/App.java
+++ gui-engine/src/main/java/ru/vsu/cs/cg/App.java
@@ -1,13 +1,44 @@
 package ru.vsu.cs.cg;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static final String FXML_PATH = "/gui.fxml";
+    private static final String WINDOW_TITLE = "3d-viewer";
+    private static final int WINDOW_WIDTH = 1400;
+    private static final int WINDOW_HEIGHT = 800;
+    private static final int MIN_WIDTH = 1200;
+    private static final int MIN_HEIGHT = 700;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_PATH));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+            primaryStage.setTitle(WINDOW_TITLE);
+            primaryStage.setScene(scene);
+            primaryStage.setMinWidth(MIN_WIDTH);
+            primaryStage.setMinHeight(MIN_HEIGHT);
+            primaryStage.show();
+        } catch (Exception e) {
+            handleStartupError(e);
+        }
+    }
+
+    private void handleStartupError(Exception e) {
+        System.err.println("Ошибка при запуске приложения:");
+        e.printStackTrace();
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
