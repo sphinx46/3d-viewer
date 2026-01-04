f2151a9aca856701985fe991953a92b02daa9eb3
Senpaka <egorkazakov01012007@mail.ru>
2026-01-04 17:13:47 +0300

    Сделана тестовая версия триангуляции. Сам метод триангуляции находится в классе Polygon, а вызывается в классе Model, где и хранится в виде <Кеша>

--- src/main/java/ru/vsu/cs/cg/model/Model.java
+++ src/main/java/ru/vsu/cs/cg/model/Model.java
@@ -11,4 +11,49 @@ public final class Model {
     public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
     public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
     public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+
+    private ArrayList<Polygon> triangulatedPolygon = null;
+    private boolean needsTriangulation = true;
+
+    /**
+     * Добавляет полигон в модель
+     * говорит что нужно пересчитать триангуляцию
+     * @param polygon добавляемый полигон
+     */
+    public void addPolygon(Polygon polygon){
+        polygons.add(polygon);
+        needsTriangulation = true;
+    }
+
+    /**
+     * Возвращает полигоны после триангуляции
+     * при необходимости пересчитывает их
+     * @return полигоны после триангуляции
+     */
+    public ArrayList<Polygon> getTriangulatedPolygon(){
+        if (needsTriangulation || triangulatedPolygon == null){
+            computeTriangulation();
+        }
+        return triangulatedPolygon;
+    }
+
+    /**
+     * Делает триангуляцию для каждого полигона
+     */
+    private void computeTriangulation(){
+        triangulatedPolygon = new ArrayList<>();
+
+        for(Polygon polygon: polygons){
+            triangulatedPolygon.addAll(polygon.triangulate());
+        }
+
+        needsTriangulation = false;
+    }
+
+    /**
+     * Если требуется произвести триангуляцию
+     */
+    public void markForTriangulation() {
+        needsTriangulation = true;
+    }
 }
