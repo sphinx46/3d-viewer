abffdb717fbb6c8488668fc65f7ebc7feb14359f
Senpaka <egorkazakov01012007@mail.ru>
2026-01-12 01:35:05 +0300

    refactor: Добавлена возможность указать цвет и использовать или нет полигональную сетку

--- core-engine/src/main/java/ru/vsu/cs/cg/rasterization/RasterizerSettings.java
+++ core-engine/src/main/java/ru/vsu/cs/cg/rasterization/RasterizerSettings.java
@@ -9,19 +9,31 @@ import javafx.scene.paint.Color;
 public class RasterizerSettings {
     private boolean useTexture;
     private boolean useLighting;
+    private boolean drawPolygonalGrid;
+    private Color gridColor;
     private Color defaultColor;
 
-    public RasterizerSettings(boolean useTexture, boolean useLighting, Color defaultColor) {
+    public RasterizerSettings(boolean useTexture, boolean useLighting, boolean drawPolygonalGrid, Color defaultColor, Color gridColor) {
         this.useTexture = useTexture;
         this.useLighting = useLighting;
+        this.drawPolygonalGrid = drawPolygonalGrid;
         this.defaultColor = defaultColor;
+        this.gridColor = gridColor;
     }
 
     public RasterizerSettings(){
         this.useTexture = false;
         this.useLighting = false;
+        this.drawPolygonalGrid = false;
         this.defaultColor = Color.GRAY;
+        this.gridColor = Color.BLACK;
     }
+
+    public Color getGridColor() {return gridColor;}
+    public void setGridColor(Color gridColor) {this.gridColor = gridColor;}
+
+    public boolean isDrawPolygonalGrid() {return drawPolygonalGrid;}
+    public void setDrawPolygonalGrid(boolean drawPolygonalGrid){ this.drawPolygonalGrid = drawPolygonalGrid;}
 
     public boolean isUseTexture() { return useTexture; }
     public void setUseTexture(boolean useTexture) { this.useTexture = useTexture; }
