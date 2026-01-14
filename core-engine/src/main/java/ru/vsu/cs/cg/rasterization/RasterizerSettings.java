462e1bc885ada3f3c7eccd7514bbd9125e9756b3
Senpaka <egorkazakov01012007@mail.ru>
2026-01-14 18:40:12 +0300

    refactor: добавлена настройка отрисовки осей в модели

--- core-engine/src/main/java/ru/vsu/cs/cg/rasterization/RasterizerSettings.java
+++ core-engine/src/main/java/ru/vsu/cs/cg/rasterization/RasterizerSettings.java
@@ -10,30 +10,37 @@ public class RasterizerSettings {
     private boolean useTexture;
     private boolean useLighting;
     private boolean drawPolygonalGrid;
+    private boolean drawAxisLines;
     private Color gridColor;
     private Color defaultColor;
 
-    public RasterizerSettings(boolean useTexture, boolean useLighting, boolean drawPolygonalGrid, Color defaultColor, Color gridColor) {
+    public RasterizerSettings(boolean useTexture, boolean useLighting,
+                              boolean drawPolygonalGrid, boolean drawAxisLines,
+                              Color defaultColor, Color gridColor) {
         this.useTexture = useTexture;
         this.useLighting = useLighting;
         this.drawPolygonalGrid = drawPolygonalGrid;
+        this.drawAxisLines = drawAxisLines;
         this.defaultColor = defaultColor;
         this.gridColor = gridColor;
     }
 
-    public RasterizerSettings(){
+    public RasterizerSettings() {
         this.useTexture = false;
         this.useLighting = false;
         this.drawPolygonalGrid = false;
+        this.drawAxisLines = false;
         this.defaultColor = Color.GRAY;
         this.gridColor = Color.BLACK;
     }
 
-    public Color getGridColor() {return gridColor;}
-    public void setGridColor(Color gridColor) {this.gridColor = gridColor;}
+    public Color getGridColor() { return gridColor; }
+    public void setGridColor(Color gridColor) { this.gridColor = gridColor; }
 
-    public boolean isDrawPolygonalGrid() {return drawPolygonalGrid;}
-    public void setDrawPolygonalGrid(boolean drawPolygonalGrid){ this.drawPolygonalGrid = drawPolygonalGrid;}
+    public boolean isDrawPolygonalGrid() { return drawPolygonalGrid; }
+    public void setDrawPolygonalGrid(boolean drawPolygonalGrid) {
+        this.drawPolygonalGrid = drawPolygonalGrid;
+    }
 
     public boolean isUseTexture() { return useTexture; }
     public void setUseTexture(boolean useTexture) { this.useTexture = useTexture; }
@@ -43,4 +50,19 @@ public class RasterizerSettings {
 
     public Color getDefaultColor() { return defaultColor; }
     public void setDefaultColor(Color defaultColor) { this.defaultColor = defaultColor; }
+
+    public boolean isDrawAxisLines() { return drawAxisLines; }
+    public void setDrawAxisLines(boolean drawAxisLines) {
+        this.drawAxisLines = drawAxisLines;
+    }
+
+    public RasterizerSettings copy() {
+        return new RasterizerSettings(
+                this.isUseTexture(),
+                this.isUseLighting(),
+                this.isDrawPolygonalGrid(),
+                this.isDrawAxisLines(),
+                this.getDefaultColor(),
+                this.getGridColor());
+    }
 }
